381125f114de17bd4afa7b97b6a152570eda3675
Mohamad Farid Saleh <github.farid1@gmail.com>
2024-09-04 11:49:41 +0900

    update scripts

--- resywebsite/urls.py
+++ resywebsite/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path('botui/', include('botui.urls')),
 ]
 
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
