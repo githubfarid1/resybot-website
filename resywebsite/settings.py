5f4323a5de7557b30a2fee6215d94d858c11b1cc
Mohamad Farid Saleh <github.farid1@gmail.com>
2024-09-12 14:28:11 +0900

    update scripts

--- resywebsite/settings.py
+++ resywebsite/settings.py
@@ -94,30 +94,30 @@ WSGI_APPLICATION = 'resywebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+if os.environ.get('DB_ENGINE') and os.environ.get('DB_ENGINE') == "mysql":
+    DATABASES = { 
+      'default': {
+        'ENGINE'  : 'django.db.backends.mysql', 
+        'NAME'    : os.getenv('DB_NAME'),
+        'USER'    : os.getenv('DB_USERNAME'),
+        'PASSWORD': os.getenv('DB_PASS'),
+        'HOST'    : os.getenv('DB_HOST'),
+        'PORT'    : os.getenv('DB_PORT'),
+        }, 
     }
-}
-# if os.environ.get('DB_ENGINE') and os.environ.get('DB_ENGINE') == "mysql":
-#     DATABASES = { 
-#       'default': {
-#         'ENGINE'  : 'django.db.backends.mysql', 
-#         'NAME'    : os.getenv('DB_NAME'),
-#         'USER'    : os.getenv('DB_USERNAME'),
-#         'PASSWORD': os.getenv('DB_PASS'),
-#         'HOST'    : os.getenv('DB_HOST'),
-#         'PORT'    : os.getenv('DB_PORT'),
-#         }, 
-#     }
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
